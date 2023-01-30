7eec4470a78863c6dd2803ccfe6872187c4ce754
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-01-30 10:58:54 +0100

    update doc

---
+++ src/app/modules/inugami/components/inugami-aside-menu/inugami.api.toc.ts
@@ -0,0 +1,11 @@
+import { Component, Inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inugami-api-toc',
+  templateUrl: './inugami.api.toc.html',
+  providers: []
+})
+export class InugamiApiToc {
+  constructor() {
+  }
+}
