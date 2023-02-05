bff2c2fe9b56d630f4a59e8d408355470e981b57
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-02-05 14:27:03 +0100

    add doc

---
+++ src/app/modules/inugami-maven-plugin/components/inugami_plugin_aside_menu/inugami.plugin.aside.menu.ts
@@ -0,0 +1,12 @@
+import { Component, Inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inugami-plugin-aside-menu',
+  templateUrl: './inugami.plugin.aside.menu.html',
+  providers: []
+})
+export class InugamiPluginAsideMenu {
+
+  constructor() {
+  }
+}
---
+++ src/app/modules/inugami-maven-plugin/components/inugami_plugin_toc/inugami.plugin.toc.ts
@@ -0,0 +1,12 @@
+import { Component, Inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inugami-plugin-toc',
+  templateUrl: './inugami.plugin.toc.html',
+  providers: []
+})
+export class InugamiPluginToc {
+
+  constructor() {
+  }
+}
