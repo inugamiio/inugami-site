32e8146aa84954262053fed5473b026c5e4c69df
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-02-22 18:45:49 +0100

    add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router ,ResolveEnd } from '@angular/router';
+import { TITLE } from './app.page.titles';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inugami-website';
+
+
+  constructor (private router: Router) {
+    this.router.events.subscribe(val => {
+      if(val instanceof ResolveEnd ){
+        let event = val as ResolveEnd ;
+        console.info(`${event.urlAfterRedirects}`);
+
+        let title =TITLE[event.urlAfterRedirects];
+        if(title==null){
+          title=TITLE[''];
+        }
+
+        let titleTags= document.getElementsByTagName('title');
+        titleTags[0].innerText=title;
+      }
+    })
+}
 }
