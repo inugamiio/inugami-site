126f9dde12f7cb55cfab9ebc8d951c5fe172ecdd
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-02-25 18:35:46 +0100

    add docs

---
+++ src/app/app.page.titles.ts
@@ -0,0 +1,7 @@
+export const TITLE : { [name: string]: string } = {
+    '' : 'Inugami',
+    '/inugami-project-analysis-maven-plugin' : 'inugami-project-analysis-maven-plugin',
+    '/inugami':'Inugami project',
+    '/inugami/inugami_api':'inugami_api'
+    
+}
