fef924a2f6413e0d3cec067165fe9b9e2e37c884
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-04-16 17:38:51 +0200

    DOC: add doc

---
+++ src/app/modules/inugami/views/inugami_commons_spring/inugami_commons_spring_toc.ts
@@ -0,0 +1,12 @@
+import { Component, Inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inugami-commons-spring-toc',
+  templateUrl: './inugami_commons_spring_toc.html',
+  providers: []
+})
+export class InugamiCommonsSpringToc {
+
+  constructor() {
+  }
+}
